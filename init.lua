03f257cfad2a01521fe6190193bec552cf2a72b4
Aryan Dalal <aryan.dalal2948@gmail.com>
2026-02-18 00:59:18 -0400

    Added the zls configuration manually as Mason only has version 15.1 of the ZLS

--- init.lua
+++ init.lua
@@ -602,6 +602,7 @@ require('lazy').setup({
         -- pyright = {},
         -- rust_analyzer = {},
         --
+        -- zls = {},
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
@@ -630,6 +631,11 @@ require('lazy').setup({
         vim.lsp.config(name, server)
         vim.lsp.enable(name)
       end
+      -- Special Zig config:
+      vim.lsp.config('zls', {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable 'zls'
 
       -- Special Lua Config, as recommended by neovim help docs
       vim.lsp.config('lua_ls', {
@@ -754,7 +760,7 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        preset = 'super-tab',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -769,7 +775,7 @@ require('lazy').setup({
       completion = {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        documentation = { auto_show = true, auto_show_delay_ms = 5000 },
       },
 
       sources = {
